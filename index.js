5634cf7c038b76fc5ab698f6457e485d381064ec
Ignacio Esmite <nacho@zuplo.com>
2022-02-03 16:23:05 -0300

    do the magic

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
+const axios = require('axios');
+const token = process.env.SENTRY_TOKEN
+const run = async () => {
+    const feedbacks = await axios(options);
+    let filteredFeedback = feedbacks.data.filter((fb) => Date.parse(fb.dateCreated) > Date.now() - 3600000*2);
+    filteredFeedback = filteredFeedback.map((fb) => ({
+       event: fb.issue.permalink,
+       email: fb.email,
+       comments: fb.comments,
+       dateCreated: fb.dateCreated
+    }));
+    return filteredFeedback;
+}
+var options = {
+  url: 'https://sentry.io/api/0/projects/\zuplo/portal/user-feedback/',
+  headers: {
+    'Authorization': `Bearer ${token}`
+  }
+};
 
 try {
   // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  const result = run();
+  //const nameToGreet = core.getInput('who-to-greet');
+  core.setOutput("payload", result);
 } catch (error) {
   core.setFailed(error.message);
 }
+
