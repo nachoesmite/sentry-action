08e0d20b242711cc4d8ff5f8c44513bd6e93a091
Ignacio Esmite <nacho@zuplo.com>
2022-02-04 10:48:08 -0300

    log results

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const core = require('@actions/core');
 const axios = require('axios');
-const token = process.env.SENTRY_TOKEN
 const run = async () => {
+    const token = process.env.SENTRY_TOKEN
+    const options = {
+      url: 'https://sentry.io/api/0/projects/\zuplo/portal/user-feedback/',
+      headers: {
+        'Authorization': `Bearer ${token}`
+      }
+    };
     const feedbacks = await axios(options);
     let filteredFeedback = feedbacks.data.filter((fb) => Date.parse(fb.dateCreated) > Date.now() - core.getInput('time-ms-period'));
     filteredFeedback = filteredFeedback.map((fb) => ({
@@ -10,14 +16,10 @@ const run = async () => {
        comments: fb.comments,
        dateCreated: fb.dateCreated
     }));
+    console.log({ events: filteredFeedback});
     core.setOutput("payload", { events: filteredFeedback});
 }
-var options = {
-  url: 'https://sentry.io/api/0/projects/\zuplo/portal/user-feedback/',
-  headers: {
-    'Authorization': `Bearer ${token}`
-  }
-};
+
 
 try {
   // `who-to-greet` input defined in action metadata file
