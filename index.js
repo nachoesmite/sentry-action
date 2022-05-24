24199fda5b465832c4edddb255af63db53395425
Nacho Esmite <nacho@zuplo.com>
2022-05-24 10:32:57 -0300

    Add dev-portal support

--- index.js
+++ index.js
@@ -1,27 +1,33 @@
 const core = require('@actions/core');
 const axios = require('axios');
 const run = async () => {
-    const token = process.env.SENTRY_TOKEN
-    const options = {
-      url: 'https://sentry.io/api/0/projects/\zuplo/portal/user-feedback/',
-      headers: {
-        'Authorization': `Bearer ${token}`
-      }
-    };
-    const feedbacks = await axios(options);
-    let filteredFeedback = feedbacks.data.filter((fb) => Date.parse(fb.dateCreated) > Date.now() - core.getInput('time-ms-period'));
-    filteredFeedback = filteredFeedback.map((fb) => ({
-       event: fb.issue.permalink,
-       email: fb.email,
-       comments: fb.comments,
-       dateCreated: fb.dateCreated
-    }));
-    console.log({ events: filteredFeedback});
+    let filteredFeedback = await getFeedbackItems('portal');
+    filteredFeedback = filteredFeedback.concat(await getFeedbackItems('dev-portal'));
     core.setOutput("payload", { events: filteredFeedback});
     core.setOutput("length", filteredFeedback.length);
-
 }
 
+
+const getFeedbackItems = async (project) => {
+  const token = process.env.SENTRY_TOKEN
+  const options = {
+    url: `https://sentry.io/api/0/projects/\zuplo/${project}/user-feedback/`,
+    headers: {
+      'Authorization': `Bearer ${token}`
+    }
+  };
+  const feedbacks = await axios(options);
+  let filteredFeedback = feedbacks.data.filter((fb) => Date.parse(fb.dateCreated) > Date.now() - core.getInput('time-ms-period'));
+  filteredFeedback = filteredFeedback.map((fb) => ({
+     event: fb.issue.permalink,
+     email: fb.email,
+     comments: fb.comments,
+     dateCreated: fb.dateCreated
+  }));
+  console.log(`Project: ${project}`);
+  console.log({ events: filteredFeedback});
+  return filteredFeedback;
+}
 
 try {
   run();
